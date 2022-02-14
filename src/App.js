e7ca8448b5b90b0a47303b01e5626459cf4405e8
robert_bao <ibaochen@gmail.com>
2022-02-14 14:44:36 -0500

    Add basic frontend UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { ThemeProvider } from "@mui/material/styles";
+import { themeOptions } from "./theme";
+import CssBaseline from "@mui/material/CssBaseline";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ThemeProvider theme={themeOptions}>
+            <CssBaseline />
+
+            <Box
+                sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    width: "100%",
+                    minHeight: "100vh",
+                    flexGrow: 1,
+                    padding: "32px",
+                    backgroundColor: themeOptions.palette.background.main,
+                }}
+            >
+                <Box
+                    sx={{
+                        width: "40%",
+                        display: "flex",
+                        flexDirection: "column",
+                    }}
+                >
+                    <Box sx={{ height: "30%" }}>
+                        <Typography
+                            variant="h2"
+                            sx={{ width: "100%", textAlign: "left" }}
+                        >
+                            The Search Toolkit
+                        </Typography>
+                        <Typography
+                            variant="h5"
+                            sx={{
+                                width: "80%",
+                                textAlign: "left",
+                                marginY: "32px",
+                            }}
+                        >
+                            Haven't found what you want on the web? Use this app
+                            to refine your searches.
+                        </Typography>
+                    </Box>
+                    <Box>
+                        <Typography variant="h4">My Searches</Typography>
+                    </Box>
+                </Box>
+                <Box
+                    sx={{
+                        paddingX: "32px",
+                        width: "60%",
+                        backgroundColor: themeOptions.palette.primary.main,
+                    }}
+                >
+                    <h1>The search param form</h1>
+                </Box>
+            </Box>
+        </ThemeProvider>
+    );
 }
 
 export default App;

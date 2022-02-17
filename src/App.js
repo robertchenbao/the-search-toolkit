96d2a3abe92d1135d599f585660c8c4047b30df3
robert_bao <ibaochen@gmail.com>
2022-02-16 22:25:00 -0500

    Add all search fields

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ import Button from "@mui/material/Button";
 function App() {
     const googleForm = useFormik({
         initialValues: {
-            mainQuery: "",
-            exactQuery: "",
-            excludedQuery: "",
+            mainKeywords: "",
+            exactKeywords: "",
+            excludedKeywords: "",
             siteName: "",
             fileType: "",
         },
@@ -21,6 +21,22 @@ function App() {
             console.log("values after submit", values);
         },
     });
+
+    const formInputStyles = {
+        width: "100%",
+        backgroundColor: themeOptions.palette.background.main,
+        fontSize: "20px",
+    };
+    const formLabelStyles = {
+        color: themeOptions.palette.text.primary,
+        width: "30%",
+    };
+
+    const inputRowStyles = {
+        display: "flex",
+        flexDirection: "row",
+        marginY: "36px",
+    };
 
     return (
         <ThemeProvider theme={themeOptions}>
@@ -46,26 +62,27 @@ function App() {
                 >
                     <Box sx={{ height: "200px" }}>
                         <Typography
-                            variant="h2"
+                            variant="h3"
                             sx={{ width: "100%", textAlign: "left" }}
                         >
                             The Search Toolkit
                         </Typography>
                         <Typography
-                            variant="h5"
+                            variant="h6"
                             sx={{
                                 width: "90%",
                                 textAlign: "left",
-                                marginY: "32px",
+                                marginY: "48px",
                             }}
                         >
                             <i>
-                                Haven't found what you want on the web? Use this
-                                app to refine your searches.
+                                Couldn't find what you want on the web? <br />
+                                Use this app to make your search even more
+                                precise.
                             </i>
                         </Typography>
                     </Box>
-                    <Box sx={{ flexGrow: 1 }}>
+                    <Box sx={{ flexGrow: 1, marginY: "48px" }}>
                         <Typography variant="h4">My Searches</Typography>
                     </Box>
                 </Box>
@@ -85,40 +102,79 @@ function App() {
                             marginY: "16px",
                         }}
                     >
-                        Create your precise search
+                        My Precise search
                     </Typography>
                     <form onSubmit={googleForm.handleSubmit}>
-                        <Box
-                            sx={{
-                                display: "flex",
-                                flexDirection: "row",
-                                marginY: "24px",
-                            }}
-                        >
-                            <Typography
-                                variant="h6"
-                                sx={{
-                                    color: themeOptions.palette.text.primary,
-                                    width: "30%",
-                                }}
-                            >
+                        <Box sx={inputRowStyles}>
+                            <Typography variant="h6" sx={formLabelStyles}>
                                 Keywords
                             </Typography>
                             <InputBase
                                 placeholder="Enter your search keywords here..."
-                                inputProps={{ "aria-label": "search" }}
-                                sx={{
-                                    width: "100%",
-                                    backgroundColor:
-                                        themeOptions.palette.background.main,
-                                    fontSize: "20px",
-                                }}
+                                sx={formInputStyles}
                                 type="text"
+                                name="mainKeywords"
+                                autoComplete="off"
                                 onChange={googleForm.handleChange}
-                                value={googleForm.values.title}
+                                value={googleForm.values.mainKeywords}
                             />
                         </Box>
-
+                        <Box sx={inputRowStyles}>
+                            <Typography variant="h6" sx={formLabelStyles}>
+                                Exact Keywords
+                            </Typography>
+                            <InputBase
+                                placeholder="Enter your exact search keywords here..."
+                                sx={formInputStyles}
+                                type="text"
+                                name="exactKeywords"
+                                autoComplete="off"
+                                onChange={googleForm.handleChange}
+                                value={googleForm.values.exactKeywords}
+                            />
+                        </Box>
+                        <Box sx={inputRowStyles}>
+                            <Typography variant="h6" sx={formLabelStyles}>
+                                Exclude
+                            </Typography>
+                            <InputBase
+                                placeholder="Exclude these keywords..."
+                                sx={formInputStyles}
+                                type="text"
+                                name="excludedKeywords"
+                                autoComplete="off"
+                                onChange={googleForm.handleChange}
+                                value={googleForm.values.excludedKeywords}
+                            />
+                        </Box>
+                        <Box sx={inputRowStyles}>
+                            <Typography variant="h6" sx={formLabelStyles}>
+                                Specific Sites
+                            </Typography>
+                            <InputBase
+                                placeholder="Search on these specific sites..."
+                                sx={formInputStyles}
+                                type="text"
+                                name="siteName"
+                                autoComplete="off"
+                                onChange={googleForm.handleChange}
+                                value={googleForm.values.siteName}
+                            />
+                        </Box>
+                        <Box sx={inputRowStyles}>
+                            <Typography variant="h6" sx={formLabelStyles}>
+                                File Type
+                            </Typography>
+                            <InputBase
+                                placeholder="Only view specific file types..."
+                                sx={formInputStyles}
+                                type="text"
+                                name="fileType"
+                                autoComplete="off"
+                                onChange={googleForm.handleChange}
+                                value={googleForm.values.fileType}
+                            />
+                        </Box>
                         <Box sx={{ display: "flex", justifyContent: "center" }}>
                             <Button
                                 size="large"
